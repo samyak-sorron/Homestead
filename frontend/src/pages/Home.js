bef5a62d41b93110e953269f835f85e41d3e0722
samyak-sorron <samyak.sharma.758@gmail.com>
2023-10-01 22:53:56 +0530

    can begin further

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,6 @@
-import { Card, Dropdown } from "../components"
+import { Dropdown } from "../components"
 import { useState } from 'react'
+import {FiSearch} from 'react-icons/fi'
 
 export default function Home() {
 
@@ -9,21 +10,24 @@ export default function Home() {
     <div>
       <nav className='flex justify-between p-5 align-middle'>
         <div>
-          <h3>Homestead</h3>
+          <h3 className="text-xl">Homestead</h3>
         </div>
-        <div  className='px-3'>
+        <div  className='flex justify-evenly px-3'>
+          <div className="inline-flex w-full justify-center gap-x-1.5 px-3 py-2 text-xl font-semibold text-gray-900 ring-gray-300 hover:bg-gray-100"><FiSearch/></div>
           <Dropdown
             logedin={false}
           />
         </div>
       </nav>
       <hr/>
-      <Card
+
+
+      {/* <Card
         id={1}
         name={'heck'}
         prompt="2 dogs under the street light"
         photo={null}
-      />
+      /> */}
     </div>
   )
 }
