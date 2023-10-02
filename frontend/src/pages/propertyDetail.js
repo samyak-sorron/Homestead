adabe6eb8cda7955d6487323999548f1045ca955
samyak-sorron <samyak.sharma.758@gmail.com>
2023-10-02 21:22:21 +0530

    property details

--- frontend/src/pages/propertyDetail.js
+++ frontend/src/pages/propertyDetail.js
@@ -1,4 +1,5 @@
 import React,{useEffect, useState} from 'react'
+import {Navbar} from '../components'
 
 const PropertyDetail = () => {
     useEffect(()=>{
@@ -6,11 +7,16 @@ const PropertyDetail = () => {
     })
   return (
     <div>
-        <div className='flex'>
-           <img className='w-6 h-6 object-cover rounded-xl columns-4' src='' alt=''></img> 
+        <Navbar/>
+        <div className='column-1 px-rm pt-18 container mx-auto md:columns-3 md:gap-8'>
+           <img className='w-full h-auto object-cover rounded-xl columns-4 mb-8' src='https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&w=1600' alt='hehe'></img> 
+           <div>
+            <h1>Samyak</h1>
+            <p>location</p>
+           </div>
         </div>
     </div>
   )
 }
 
-export default PropertyDetail
+export default PropertyDetail;
