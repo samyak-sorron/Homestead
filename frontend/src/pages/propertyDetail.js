6d49af962ff1adbf63d58a75e67c0f3e15e957bd
samyak-sorron <samyak.sharma.758@gmail.com>
2023-11-03 15:31:55 +0530

    property details on work

--- frontend/src/pages/propertyDetail.js
+++ frontend/src/pages/propertyDetail.js
@@ -2,7 +2,7 @@ import React,{useEffect, useState} from 'react'
 import {Navbar} from '../components'
 import {Link, useParams} from "react-router-dom";
 
-import { Container, Card, Heading, Text } from 'tailwindcss-react-kit';
+// import { div, div, Heading, p } from 'tailwindcss-react-kit';
 // import BookingWidget from "../BookingWidget";
 // import PlaceGallery from "../PlaceGallery";
 // import AddressLink from "../AddressLink";
@@ -34,7 +34,7 @@ const PropertyDetail = () => {
   return (
     <div>
     <Navbar/>
-    <div className="grid grid-cols-4 px-20">
+    <div className="grid grid-cols-4 px-20 gap-10">
       <div className=''>
         <img src={house1} alt='house' style={{"height":"50vh", "width":"50vh"}}/>
       </div>
@@ -70,23 +70,23 @@ const PropertyDetail = () => {
         </div>
       </div>
     </div>
-    <div>
-    <Container className="my-8">
-      <Card className="text-center">
-        <Heading>Standard</Heading>
-        <Text>
+    <div >
+    <div className="my-8 border-2 rounded-md float-right" style={{"width":"40vh"}}>
+      <div className="text-center">
+        <h3>Standard</h3>
+        <p>
           <span className="text-4xl font-bold">$9</span>
           <span className="text-gray-500">/mo</span>
-        </Text>
-        <Text className="mt-4">Unlimited Storage</Text>
-        <Text>25GB Bandwidth</Text>
-        <Text>10 Databases</Text>
-        <Text>500 GB SSD Disk</Text>
+        </p>
+        <p className="mt-4">Unlimited Storage</p>
+        <p>25GB Bandwidth</p>
+        <p>10 Databases</p>
+        <p>500 GB SSD Disk</p>
         <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Get Started
         </button>
-      </Card>
-    </Container>
+      </div>
+    </div>
     </div>
     </div>
   );
